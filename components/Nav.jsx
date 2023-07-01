52924d2463517edcce074e5e3bc8255cf0e16ade
Brandon Ban Kai Xian <bbkx226@gmail.com>
2023-07-01 18:35:22 +0800

    Implement Prompt Posting System

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -37,7 +37,7 @@ const Nav = () => {
       <div className="sm:flex hidden">
         {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
-            <Link href="/create" className="black_btn">Create Post</Link>
+            <Link href="/create-prompt" className="black_btn">Create Post</Link>
             <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
