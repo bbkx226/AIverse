a216353be1880946b25f7ad38d79a2d8d914a1f6
Brandon Ban Kai Xian <bbkx226@gmail.com>
2023-07-02 17:18:23 +0800

    Implement v1.1.4

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,10 +5,23 @@ import User from "@models/user";
 export const GET = async (req) => {
     try {
         await connectToDB();
+        const prompts = await Prompt.find().populate({
+          path: "creator"
+        });
 
-        const prompts = await Prompt.find({}).populate('creator');
+        const response = new Response(JSON.stringify(prompts), {
+          status: 200,
+        });
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        // Add a unique identifier to the URL to force a cache-busting reload
+        const url = new URL(request.url);
+        url.searchParams.set("t", Date.now());
+        response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.headers.set("Pragma", "no-cache");
+        response.headers.set("Expires", "0");
+        response.headers.set("Location", url.toString());
+
+        return response;
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
